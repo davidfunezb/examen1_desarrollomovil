7cd2a9d92ace4afe3f21cef10c5b95f81ddfb005
davidfunezb <davidarturo9@unitec.edu>
2026-08-17 22:18:57 -0600

    agregar imagen a la cabecera

--- Componentes/Cabecera.tsx
+++ Componentes/Cabecera.tsx
@@ -1,8 +1,12 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 export const Cabecera = () => {
     return (
         <View style={estilos.contenedor}>
+            <Image
+                source={require('../assets/cocinero.png')}
+                style={estilos.imagen}
+            />
             <Text style={estilos.titulo}>Recetas</Text>
             <Text style={estilos.descripcion}>
                 La aplicación permite a los usuarios agregar nuevas recetas,
@@ -19,4 +23,5 @@ const estilos = StyleSheet.create({
     titulo: { fontSize: 22, fontWeight: 'bold', marginBottom: 8 },
     descripcion: { textAlign: 'center', color: '#444', marginBottom: 8 },
     dueno: { fontSize: 12, color: '#888' },
+    imagen: { width: 100, height: 100, marginBottom: 8 },
 });
--- Providers/ProviderReceta.tsx
+++ Providers/ProviderReceta.tsx
@@ -16,6 +16,37 @@ export const ProviderReceta = ({ children }: ViewReact) => {
     ingrediente5: 'agua',
     fecha: '10/08/2026',
 },
+{
+        id: '2',
+        nombre: 'Baleada',
+        ingrediente1: 'tortilla',
+        ingrediente2: 'frijoles',
+        ingrediente3: 'queso',
+        ingrediente4: 'crema',
+        ingrediente5: 'mantequilla',
+        fecha: '12/08/2026',
+    },
+    {
+        id: '3',
+        nombre: 'Pollo con tajadas',
+        ingrediente1: 'pollo',
+        ingrediente2: 'plátano',
+        ingrediente3: 'repollo',
+        ingrediente4: 'tomate',
+        ingrediente5: 'limón',
+        fecha: '15/08/2026',
+    },
+    {
+        id: '4',
+        nombre: 'ceviche de curiles',
+        ingrediente1: 'Curiles frescos',
+        ingrediente2: 'cebolla morada',
+        ingrediente3: 'tomate',
+        ingrediente4: 'salsa inglesa',
+        ingrediente5: 'jugo de limon',
+        fecha: '17/08/2026',
+    },
+
     ]);
 
     const [recetaSeleccionada, setRecetaSeleccionada] = useState<Receta | null>(null);
